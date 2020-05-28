74b5263e33559f028ac5b0a8cf7827d061150934
Bhavesh <bhaveshsingh022000@hotmail.com>
2020-05-28 09:55:56 +0530

    added navigation bar

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const boydParser = require('body-parser');
 
 const homeRoutes = require('./routes/home');
 
 app.set('view engine','ejs');
 app.set('views','views');
+
+app.use(boydParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
+
 
 app.use(homeRoutes);
 
