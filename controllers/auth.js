9cc1c17f1231e8c3534f273c85c551b16104fee9
Bhavesh <bhaveshsingh022000@hotmail.com>
2020-05-28 12:06:41 +0530

    added login functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,30 +1,57 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
+
 
 exports.postSignup = (req,res,next)=>{
     const email = req.body.email;
     const name = req.body.name;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
-    console.log(email);
-    console.log(email);
+    console.log(confirmPassword);
+    console.log(password);
     User.findOne({email: email})
     .then(userDoc =>{
         if(userDoc){
-            return res.redirect('/signup');
+            return res.redirect('/');
         }
-        const user = new User({
-            email:email,
-            name:name,
-            password:password,
-            contacts: []
-        });
-        return user.save();
-    })
-    .then(result => {
-        res.redirect('/home');
+        return bcrypt.hash(password, 12)
+        .then(hashedPassword => {
+            const user = new User({
+                email:email,
+                name:name,
+                password:hashedPassword,
+                contacts: []
+            });
+            return user.save();
+        })
+        .then(result => {
+            res.redirect('/');
+        })
     })
     .catch(err => {
         console.log(err);
     })
+};
 
+exports.postLogin = (req, res ,next)=>{
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({email:email})
+    .then(user =>{
+        if(!user){
+            return res.redirect('/');
+        }
+        bcrypt.compare(password, user.password)
+            .then(result => {
+                if(result){
+                    return res.redirect('/home');
+                }
+                return res.redirect('/');
+            })
+            .catch(err => {
+                res.redirect('/');
+            })
+    })
+    
 };
--- routes/auth.js
+++ routes/auth.js
@@ -6,4 +6,6 @@ const authController = require('../controllers/auth');
 
 router.post('/signup',authController.postSignup);
 
+router.post('/login',authController.postLogin);
+
 module.exports = router;
