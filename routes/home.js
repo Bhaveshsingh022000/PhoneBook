9602ab0b327cbd91b48cff18c1296605e2451c5e
Bhavesh <bhaveshsingh022000@hotmail.com>
2020-05-28 13:10:41 +0530

    improved route protection

--- routes/home.js
+++ routes/home.js
@@ -3,8 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const homeController = require('../controllers/home');
+const isAuth = require('../middleware/is-auth');
 
 router.get('/', homeController.getLanding);
-router.get('/home',homeController.getHomepage);
+router.get('/home',isAuth,homeController.getHomepage);
 
 module.exports = router;
