c0c87be97d185a9888021b4ac8ace646bf59dc11
Bhavesh <bhaveshsingh022000@hotmail.com>
2020-05-28 22:56:37 +0530

    added pagination

--- controllers/home.js
+++ controllers/home.js
@@ -1,16 +1,34 @@
 const Contact = require('../models/contact');
+
+const Items_per_Page = 4;
+let totalItem;
+
 exports.getHomepage = (req, res, next) => {
-    Contact.find({user:req.session.user})
-    .then(contacts => {
-        // console.log(contacts);
-        res.render('home',{
-            pageTitle:'Home',
-            contacts: contacts
-        });
-    })
-    .catch(err => {
-        console.log(err);
-    })
+    const page = +req.query.page || 1;
+    Contact.find({ user: req.session.user })
+        .countDocuments()
+        .then(num => {
+            totalItem = num;
+            return Contact.find({ user: req.session.user })
+                .skip((page - 1) * Items_per_Page)
+                .limit(Items_per_Page)
+        })
+        .then(contacts => {
+            // console.log(contacts);
+            res.render('home', {
+                pageTitle: 'Home',
+                contacts: contacts,
+                currentPage: page,
+                hasNextPage: Items_per_Page * page < totalItem,
+                hasPreviousPage: page > 1,
+                nextPage: page+1,
+                previousPage : page - 1,
+                lastPage: Math.ceil(totalItem/Items_per_Page)
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        })
 };
 
 exports.getLanding = (req, res, next) => {
@@ -25,8 +43,8 @@ exports.postAddContact = (req, res, next) => {
     const result = JSON.parse(req.body.data);
     console.log(result);
     const image = req.file;
-    
-    if(!image){
+
+    if (!image) {
         res.redirect('/addContact');
     }
     const imageUrl = image.path;
@@ -40,80 +58,80 @@ exports.postAddContact = (req, res, next) => {
         imageUrl: imageUrl
     });
     contact.save()
-    .then(output => {
-        res.redirect('/home');
-    })
-    .catch(error =>{
-        console.log(error);
-    })
+        .then(output => {
+            res.redirect('/home');
+        })
+        .catch(error => {
+            console.log(error);
+        })
 };
 
-exports.getEditContact = (req,res,next)=>{
+exports.getEditContact = (req, res, next) => {
     const id = req.params.id;
     // console.log(id);
     Contact.findById(id)
-    .then(result => {
-        res.status(201).json({
-            message: 'Contact fetched',
-            contact: result
+        .then(result => {
+            res.status(201).json({
+                message: 'Contact fetched',
+                contact: result
+            })
         })
-    })
-    .catch(err =>{
-        console.log(err);
-    })
+        .catch(err => {
+            console.log(err);
+        })
 };
 
-exports.postEditContact = (req, res, next)=>{
+exports.postEditContact = (req, res, next) => {
     const result = JSON.parse(req.body.data);
     const id = req.body.id;
     const image = req.file;
-    if(!image){
+    if (!image) {
         Contact.findById(id)
-        .then(contact =>{
-            contact.name = result.name;
-            contact.listemail = result.email;
-            contact.listphoneno = result.phone;
-            contact.dateofbirth = result.dob;
-            return contact.save();
-        })
-        .then(result =>{
-            console.log("contact updated");
-            res.redirect('/home');
-        })
-        .catch(err =>{
-            console.log(err);
-        })
+            .then(contact => {
+                contact.name = result.name;
+                contact.listemail = result.email;
+                contact.listphoneno = result.phone;
+                contact.dateofbirth = result.dob;
+                return contact.save();
+            })
+            .then(result => {
+                console.log("contact updated");
+                res.redirect('/home');
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
-    else{
+    else {
         Contact.findById(id)
-        .then(contact =>{
-            contact.name = result.name;
-            contact.listemail = result.email;
-            contact.listphoneno = result.phone;
-            contact.dateofbirth = result.dob;
-            contact.imageUrl = image.path;
-            return contact.save();
-        })
-        .then(result =>{
-            console.log("contact updated");
-            res.redirect('/home');
-        })
-        .catch(err =>{
-            console.log(err);
-        })
+            .then(contact => {
+                contact.name = result.name;
+                contact.listemail = result.email;
+                contact.listphoneno = result.phone;
+                contact.dateofbirth = result.dob;
+                contact.imageUrl = image.path;
+                return contact.save();
+            })
+            .then(result => {
+                console.log("contact updated");
+                res.redirect('/home');
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
-    
+
 }
 
-exports.deleteContact = (req,res,next)=>{
+exports.deleteContact = (req, res, next) => {
     const id = req.params.id;
     Contact.findByIdAndRemove(id)
-    .then(result =>{
-        console.log("Delete success");
-        // res.status(202).json({message:'Delete Success'});
-        res.redirect('/home');
-    })
-    .catch(err => {
-        console.log(err);
-    })
+        .then(result => {
+            console.log("Delete success");
+            // res.status(202).json({message:'Delete Success'});
+            res.redirect('/home');
+        })
+        .catch(err => {
+            console.log(err);
+        })
 }
