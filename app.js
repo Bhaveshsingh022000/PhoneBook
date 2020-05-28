ee7e21f9d5db473899e1c01b7555cb2fa67df160
Bhavesh <bhaveshsingh022000@hotmail.com>
2020-05-28 10:09:06 +0530

    connecting to database

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const boydParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const MONGODB_URI ='mongodb+srv://bhavesh_05:Bhavesh2017@cluster0-yuok1.mongodb.net/phonebook?retryWrites=true&w=majority';
+
 
 const homeRoutes = require('./routes/home');
 
@@ -14,4 +18,11 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.use(homeRoutes);
 
-app.listen(3000);
+mongoose.connect(MONGODB_URI)
+    .then(result =>{
+        console.log('connected');
+        app.listen(3000);
+    })
+    .catch(err =>{
+        console.log(err);
+    })
